73d09646bdd8827f01946826b249f8fd0c7a5baa
HTGoldston <83466268+HTGoldston@users.noreply.github.com>
2021-05-26 17:03:30 -0400

    Update: sweepstake, py (correction)
    typo

--- sweepstake.py
+++ sweepstake.py
@@ -2,8 +2,9 @@ class Sweepstake:
     def __init__(self, contestants, name):
         self.sweepstake = Sweepstake
         self.contestants = contestants
+        self.name = ""
 
-    def register_contstant(contestant): void
+    def register_contstant(contestant): pass
 
     def pick_winner(): pass
 
