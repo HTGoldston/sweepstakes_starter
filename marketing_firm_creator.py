a0906bfa4b3f82ad52b901d2bd4df3102d9dbe49
HTGoldston <83466268+HTGoldston@users.noreply.github.com>
2021-05-26 12:38:12 -0400

    Created class:  Marketing_Firm_Creator
    (self, choose_manager_type(): marketing_firm):

---
+++ marketing_firm_creator.py
@@ -0,0 +1,6 @@
+class Marketing_Firm_Creator:
+
+    def __init__(self, choose_manager_type(): marketing_firm):
+        self.marketing_firm_creator = Marketing_Firm_Creator
+    #?
+        self.choose_manager_type(): marketing_firm
