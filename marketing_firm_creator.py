e227a3b73d16c36305086791b7182689fec6b1fa
HTGoldston <83466268+HTGoldston@users.noreply.github.com>
2021-05-26 14:26:29 -0400

    Created class: Sweepstake
    1. (self, contestants, name)
    2. Updated Class Contestant to establish inheritance to Sweepstakes

--- marketing_firm_creator.py
+++ marketing_firm_creator.py
@@ -1,6 +1,9 @@
+from self import self
+
+
 class Marketing_Firm_Creator:
 
     def __init__(self, choose_manager_type(): marketing_firm):
         self.marketing_firm_creator = Marketing_Firm_Creator
-    #?
+    #? Question
         self.choose_manager_type(): marketing_firm
---
+++ sweepstake.py
@@ -0,0 +1,11 @@
+class Sweepstake
+    def __init__(self, contestants, name):
+        self.sweepsstake = Sweepstake
+        self.contestants = contestants
+        self.name = name
+
+    def register_contstant(contestant): void
+
+    def pick_winner():contestant:
+
+    print_contsatant_info(contestant): void
